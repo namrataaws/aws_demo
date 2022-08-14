276994fa16e6eca165ffd09ac4b3dc00638b1a1c
Harshal <harshaldaws@gmmail.com>
2022-08-14 19:17:25 +0530

    route table added

---
+++ Terraform_VPC/route_table.tf
@@ -0,0 +1,34 @@
+#Public Route Table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.gw_vpc.id
+  }
+
+
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+#Private Route Table
+/*
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  route {
+    cidr_block = "10.0.2.0/24"
+    gateway_id = aws_internet_gateway.gw_vpc.id
+  }
+
+
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+*/
+
